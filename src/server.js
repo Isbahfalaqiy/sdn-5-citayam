c4ea5e90828d7df390d9410cf22e1254e2be0721
Isbahfalaqiy <isbahfalaqiy61@gmail.com>
2025-08-10 21:37:32 +0700

    vercel.json

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const jsonServer = require("json-server");
+const path = require("path");
+
 const server = jsonServer.create();
-const router = jsonServer.router("sekolah.json");
+const router = jsonServer.router(path.join(__dirname, "..", "sekolah.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// Export sebagai handler untuk Vercel
+module.exports = server;
